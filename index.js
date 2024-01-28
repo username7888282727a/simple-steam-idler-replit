ee522678652de70cb2cb4cd7acce18225a9a52bc
ONURAN6969 <150477764+ONURAN6969@users.noreply.github.com>
2024-01-28 04:28:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
-var username = '';
-var password = '';
-var shared_secret = '';
+var username = process.env.username;
+var password = process.env.password;
+var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730,1172470,570];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
@@ -16,4 +16,3 @@ user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
 	user.gamesPlayed(games);
-});
