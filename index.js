6f79bf8253219cf6c2eee92b4ff874d0d35226e0
ONURAN6969 <150477764+ONURAN6969@users.noreply.github.com>
2024-01-28 05:11:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
 	user.gamesPlayed(games);
-	
+});
